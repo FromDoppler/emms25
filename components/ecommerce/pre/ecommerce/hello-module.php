9c398eb01c77bbb26f998c58a63ad9f8bc8a1916
Matías Roy Rodriguez <mroy@makingsense.com>
2025-03-14 11:13:04 -0300

    chore: update link

--- components/ecommerce/pre/ecommerce/hello-module.php
+++ components/ecommerce/pre/ecommerce/hello-module.php
@@ -7,7 +7,7 @@
         <div class="hero-registration__add-event">
             <p> AGÉNDALO EN TU CALENDARIO</p>
             <span><a class=" emms__fade-in" href="https://www.addevent.com/event/nt24784790" target="_blank">EVENTO DÍA 1</a></span>
-            <span><a class=" emms__fade-in" href="https://www.addevent.com/event/nt24784790" target="_blank">EVENTO DÍA 2</a></span>
+            <span><a class=" emms__fade-in" href="https://www.addevent.com/event/OQ24881440" target="_blank">EVENTO DÍA 2</a></span>
         </div>
     </div>
     <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/marquee.php') ?>
