bea4317ade4f8f6544a8b77484a64867b923238b
Matías Roy Rodriguez <mroy@makingsense.com>
2025-04-14 13:28:42 -0300

    fix: general fixes

--- components/schedule/speaker-card/speaker-card.php
+++ components/schedule/speaker-card/speaker-card.php
@@ -1,4 +1,4 @@
-<div class="speaker-card <?= 'speaker-card--' . $speaker['exposes'] ?>" data-target-speaker="modal-<?= $speaker['id'] ?>">>
+<div class="speaker-card <?= 'speaker-card--' . $speaker['exposes'] ?>" data-target-speaker="modal-<?= $speaker['id'] ?>">
 
     <!-- Ribbon del speaker vip -->
     <?php include('speaker-ribon.php'); ?>
