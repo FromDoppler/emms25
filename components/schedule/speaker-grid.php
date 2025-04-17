90172a8ae6ec815d51700f34fc7e037636a025c3
Matías Roy Rodriguez <mroy@makingsense.com>
2025-04-17 10:21:26 -0300

    chore: move days functions

--- components/schedule/speaker-grid.php
+++ components/schedule/speaker-grid.php
@@ -1,34 +1,8 @@
 <?php
 include_once($_SERVER['DOCUMENT_ROOT'] . '/components/schedule/schedule-tabs/schedule-tabs-helper.php');
 include_once($_SERVER['DOCUMENT_ROOT'] . '/components/schedule/speaker-card/helpers/index.php');
+include_once($_SERVER['DOCUMENT_ROOT'] . '/components/schedule/speaker-grid/grid-days.php');
 include_once($_SERVER['DOCUMENT_ROOT'] . '/components/schedule/speaker-card/speaker-modal/speaker-modal-helper.php');
-function showEventDatetimeByDay($day, $eventState)
-{
-    if ($day === 1) {
-        // TODO: Revisar logica y contenido al cambiar estado de eventos
-?>
-        <div class="emms__calendar__date emms__fade-in">
-            <div class="emms__calendar__date__country">
-                <?php if ($eventState['isPre']) : ?>
-                    <p> La transmisión comienza a las</p>
-                    <span><img src="/src/img/flag-argentina.png" alt="Argentina">(ARG) 11:00</span>.
-                    <p>Si no eres de allí o estarás en otro lado,</p> <a href="https://www.timeanddate.com/worldclock/fixedtime.html?msg=EMMS+E-commerce+D%C3%8DA+1&iso=20250428T11&p1=51&ah=6" target="_blank">mira el horario de tu país</a>
-                <?php endif ?>
-            </div>
-        </div>
-    <?php
-    } else if ($day === 2) {
-    ?>
-        <div class="emms__calendar__date emms__fade-in">
-            <div class="emms__calendar__date__country">
-                <p> La transmisión comienza a las</p>
-                <span><img src="/src/img/flag-argentina.png" alt="Argentina">(ARG) 11:00</span>.
-                <p>Si no eres de allí o estarás en otro lado,</p> <a href="https://www.timeanddate.com/worldclock/fixedtime.html?msg=EMMS+E-commerce+D%C3%8DA+2&iso=20250429T11&p1=51&ah=6" target="_blank">mira el horario de tu país</a>
-            </div>
-        </div>
-<?php
-    }
-}
 ?>
 
 <div class="emms__calendar__tabs">
@@ -53,7 +27,7 @@ function showEventDatetimeByDay($day, $eventState)
     ?>
         <div class="emms__container--lg" role="tabpanel" aria-labelledby="day<?= $dayIndex ?>">
             <?php
-            showEventDatetimeByDay($dayIndex, $ecommerceStates);
+            render_event_day($dayIndex, $ecommerceStates);
             ?>
             <div class="dk">
                 <div class="speaker-grid ">
---
+++ components/schedule/speaker-grid/grid-days.php
@@ -0,0 +1,30 @@
+<?php
+
+function render_event_day($day, $eventState)
+{
+    if ($day === 1) {
+        // TODO: Revisar logica y contenido al cambiar estado de eventos
+?>
+        <div class="emms__calendar__date emms__fade-in">
+            <div class="emms__calendar__date__country">
+                <?php if ($eventState['isPre']) : ?>
+                    <p> La transmisión comienza a las</p>
+                    <span><img src="/src/img/flag-argentina.png" alt="Argentina">(ARG) 11:00</span>.
+                    <p>Si no eres de allí o estarás en otro lado,</p> <a href="https://www.timeanddate.com/worldclock/fixedtime.html?msg=EMMS+E-commerce+D%C3%8DA+1&iso=20250428T11&p1=51&ah=6" target="_blank">mira el horario de tu país</a>
+                <?php endif ?>
+            </div>
+        </div>
+    <?php
+    } else if ($day === 2) {
+    ?>
+        <div class="emms__calendar__date emms__fade-in">
+            <div class="emms__calendar__date__country">
+                <p> La transmisión comienza a las</p>
+                <span><img src="/src/img/flag-argentina.png" alt="Argentina">(ARG) 11:00</span>.
+                <p>Si no eres de allí o estarás en otro lado,</p> <a href="https://www.timeanddate.com/worldclock/fixedtime.html?msg=EMMS+E-commerce+D%C3%8DA+2&iso=20250429T11&p1=51&ah=6" target="_blank">mira el horario de tu país</a>
+            </div>
+        </div>
+<?php
+    }
+}
+?>
