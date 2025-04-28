7c0a74eff40a862fd97131638e08eebd8098e8e0
Matías Roy Rodriguez <mroy@makingsense.com>
2025-04-28 08:11:06 -0300

    feat: add ecommerce certificate logic

---
+++ textify/certificate-emms2025qa.php
@@ -0,0 +1,48 @@
+<?php
+// Establecer el tipo de contenido
+header('Content-Type: image/png');
+header("Access-Control-Allow-Origin: https://qa.goemms.com");
+
+// Tamaño de la imagen
+$img_w = 1080;
+$img_h = 763;
+
+// El texto a dibujar
+$name = $_GET['fullname'];
+$type = $_GET['type'];
+$imgName =  'certificadoemms2025-' . $type . '.png';
+
+if ($type === 'workshop') {
+    $workshopType = $_GET['workshoptype'];
+    $workshopMap = [
+        'laurabarreto-AZC173' => 'laurabarreto',
+        'luisbetancourt-XYZ436' => 'luisbetancourt',
+        'joseanmuñoz-QWE799' => 'joseanmuñoz',
+        'xavieridevik-JKL391' => 'xavieridevik',
+        'pablomoratinos-MNO644' => 'pablomoratinos',
+        'matiascarrera-ZPH295' => 'matiascarrera',
+    ];
+    $imgName = 'certificadoemms2025-' . $workshopMap[$workshopType] . '.png';
+}
+
+// Crear la imagen
+$im = imagecreatefrompng($imgName);
+
+// Crear algunos colores
+$txt_color = imagecolorclosest($im, 48, 33, 0);
+
+// Fuentes
+$ffontProximaItalic = './fonts/proxima-nova-italic.ttf';
+$fontGotham = './fonts/gothamroundedmedium.ttf';
+
+// Centrar nombre
+$bbox_name = imagettfbbox(44,0,$fontGotham,$name);
+$bbox_name_x = $bbox_name[0] + (imagesx($im) / 2) - ($bbox_name[4] / 2);
+
+// Añadir el titulo
+imagettftext($im, 44, 0, $bbox_name_x, 600, $txt_color, $fontGotham, $name);
+
+// Usar imagepng() resultará en un texto más claro comparado con imagejpeg()
+imagepng($im);
+
+imagedestroy($im);
