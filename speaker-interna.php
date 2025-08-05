713cb7d8420de88325a9aa65fee668d09ff7c182
Matias Cappato <mcappato@makingsense.com>
2025-08-05 13:59:33 +0000

    refactor: new router system apply

--- speaker-interna.php
+++ speaker-interna.php
@@ -1,25 +1,4 @@
 <?php
-require_once($_SERVER['DOCUMENT_ROOT'] . '/config.php');
-require_once($_SERVER['DOCUMENT_ROOT'] . '/services/functions.php');
-?>
-
-<script type="module">
-    import {
-        getUrlWithParams
-    } from '/src/<?= VERSION ?>/js/common/index.js';
-    import {
-        eventsType
-    } from '/src/<?= VERSION ?>/js/enums/eventsType.enum.js';
-    import {
-        userRegisteredInEvent,
-        checkEncodeUrl
-    } from '/src/<?= VERSION ?>/js/user.js';
-    checkEncodeUrl();
-    if (!userRegisteredInEvent(eventsType.ECOMMERCE)) {
-        window.location.href = getUrlWithParams('/ecommerce');
-    }
-</script>
-<?php
-$response = processPhaseToShow(ECOMMERCE);
-require_once($_SERVER['DOCUMENT_ROOT'] . "/pages/ecommerce/$response[phaseToShow]/speaker-interna.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . '/config/app-config.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . "/pages/{$currentEvent['folder']}/{$currentPhase}/{$currentEvent['sharedPages']['speaker']['internal']['page']}");
 ?>
