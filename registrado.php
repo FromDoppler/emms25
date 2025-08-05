ba6db5be41b10b174d002205d800b4c1fcff2ea0
Matias Cappato <mcappato@makingsense.com>
2025-08-05 13:35:10 +0000

    refactor: new router system apply

--- registrado.php
+++ registrado.php
@@ -1,34 +1,4 @@
 <?php
-require_once($_SERVER['DOCUMENT_ROOT'] . '/config.php');
-require_once($_SERVER['DOCUMENT_ROOT'] . '/services/functions.php');
-?>
-<script src='/src/<?= VERSION ?>/js/vendors/socket.io.min.js?version=<?= VERSION ?>'></script>
-<script>
-    const socket = io("wss://<?= URL_REFRESH ?>", {
-        path: "/<?= PATH_REFRESH ?>/socket.io"
-    });
-    socket.on("state", (args) => {
-        location.reload();
-    });
-</script>
-<script type="module">
-    import {
-        getUrlWithParams
-    } from '/src/<?= VERSION ?>/js/common/index.js';
-    import {
-        eventsType
-    } from '/src/<?= VERSION ?>/js/enums/eventsType.enum.js';
-    import {
-        userRegisteredInEvent,
-        checkEncodeUrl
-    } from '/src/<?= VERSION ?>/js/user.js';
-    checkEncodeUrl();
-    if (!userRegisteredInEvent(eventsType.DIGITALTRENDS)) {
-        window.location.href = getUrlWithParams('/');
-    }
-</script>
-<?php
-$response = processPhaseToShow(DIGITALTRENDS);
-$isRegistered=1;
-require_once($_SERVER['DOCUMENT_ROOT'] . "/pages/digital-trends/$response[phaseToShow]/home-registrado.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . '/config/app-config.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . "/pages/{$currentEvent['folder']}/{$currentPhase}/{$currentEvent['sharedPages']['home']['registered']['page']}");
 ?>
