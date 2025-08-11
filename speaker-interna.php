85ce91cb4c536b33474285a710dbcbed4a3ec5b7
Matias Cappato <mcappato@makingsense.com>
2025-08-11 15:14:40 +0000

    fix(speaker-interna): restrict access to post phase only

--- speaker-interna.php
+++ speaker-interna.php
@@ -1,4 +1,11 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'] . '/config/app-config.php');
+
+// Si no estamos en la etapa post, redireccionar al index
+if ($currentPhase !== 'post') {
+    header('Location: /');
+    exit;
+}
+
 require_once($_SERVER['DOCUMENT_ROOT'] . "/pages/{$currentEvent['folder']}/{$currentPhase}/{$currentEvent['sharedPages']['speaker']['internal']['page']}");
 ?>
