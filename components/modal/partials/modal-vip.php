65a298e05480f04661bc77b619774bd1bfd93b74
Matías Roy Rodriguez <mroy@makingsense.com>
2025-09-09 08:15:55 -0300

    feat: modals UX improvements

--- components/modal/partials/modal-vip.php
+++ components/modal/partials/modal-vip.php
@@ -2,11 +2,15 @@
   <div class="popup-modal__vip-copy">
     <h3 id="<?= $titleId ?>" class="popup-modal__title">⭐ ¡Tu entrada VIP te espera! ⭐</h3>
     <p class="popup-modal__text">
-      Aprende con Speakers internacionales, participa en Workshops prácticos y conecta con referentes que marcan tendencia en Marketing Digital.
-      Vive el EMMS desde adentro con beneficios exclusivos solo para asistentes VIP.
+      Vive el EMMS con beneficios exclusivos solo para asistentes VIP:
     </p>
+    <ul>
+      <li>Workshops con referentes internacionales.</li>
+      <li>Cuenta gratuita en Doppler por 6 meses (válido para cuentas nuevas).</li>
+      <li>Estrategias y herramientas que transformarán tu negocio.</li>
+    </ul>
     <a href="/checkout" class="emms__cta emms__cta--terciary emms__cta--sm">ADQUIERE TU ENTRADA</a>
   </div>
 
-  <img class="popup-modal__vip-img" src="/src/img/modals/persona-corona.png" alt="Mujer feliz" aria-hidden="true" />
+  <img class="popup-modal__vip-img" src="/src/img/modals/persona-cupones.png" alt="Mujer feliz con tickets" aria-hidden="true" />
 </div>
