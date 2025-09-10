a3d207383c1c90b71cb79179e0aa928f347a5135
Matias Cappato <mcappato@makingsense.com>
2025-09-10 17:45:53 +0000

    fix(spreadsheet): handle null values and extend range

--- utils/SpreadSheetGoogle.php
+++ utils/SpreadSheetGoogle.php
@@ -3,7 +3,7 @@ require_once 'spread/write.php';
 
 class SpreadSheetGoogle
 {
-    const RANGE = 'A1:R1';
+    const RANGE = 'A1:AE1';
 
     public static function write($idSpreadSheet, $user, $db)
     {
@@ -14,19 +14,19 @@ class SpreadSheetGoogle
                 date('d-m-Y'),
                 $user['promotions'],
                 $user['privacy'],
-                $user['firstname'] . " ",
+                $user['firstname'],
                 $user['email'],
-                $user['source_utm'] . " ",
-                $user['medium_utm'] . " ",
-                $user['campaign_utm'] . " ",
-                $user['content_utm'] . " ",
-                $user['term_utm'] . " ",
-                $user['origin'] . " ",
-                $user['country_ip'] . " ",
-                $user['ecommerce'] . " ",
-                $user['digital_trends'] . " ",
-                $user['phone'] . " ",
-                $user['emms_ref'] . " ",
+                $user['source_utm'] ?? '',
+                $user['medium_utm'] ?? '',
+                $user['campaign_utm'] ?? '',
+                $user['content_utm'] ?? '',
+                $user['term_utm'] ?? '',
+                $user['origin'] ?? '',
+                $user['country_ip'] ?? '',
+                $user['ecommerce'] ?? '',
+                $user['digital_trends'] ?? '',
+                $user['phone'] ?? '',
+                $user['emms_ref'] ?? '',
             )
         );
 
@@ -46,20 +46,20 @@ class SpreadSheetGoogle
         }
 
         return array(
-            $user['stripe']['price'] . " ",
-            $user['stripe']['discount'] . " ",
-            $user['stripe']['final_price'] . " ",
-            $user['stripe']['customer_name'] . " ",
-            $user['stripe']['customer_email'] . " ",
-            $user['stripe']['customer_country'] . " ",
-            $user['stripe']['tax_id'] . " ",
-            $user['stripe']['payment_status'] . " ",
-            $user['stripe']['coupon_id'] . " ",
-            $user['stripe']['coupon_name'] . " ",
-            $user['stripe']['event_name'] . " ",
-            $user['stripe']['event_phase']  . " ",
-            $user['stripe']['ticket_name']  . " ",
-            $user['stripe']['ticket_price_id']  . " ",
+            $user['stripe']['price'] ?? '',
+            $user['stripe']['discount'] ?? '',
+            $user['stripe']['final_price'] ?? '',
+            $user['stripe']['customer_name'] ?? '',
+            $user['stripe']['customer_email'] ?? '',
+            $user['stripe']['customer_country'] ?? '',
+            $user['stripe']['tax_id'] ?? '',
+            $user['stripe']['payment_status'] ?? '',
+            $user['stripe']['coupon_id'] ?? '',
+            $user['stripe']['coupon_name'] ?? '',
+            $user['stripe']['event_name'] ?? '',
+            $user['stripe']['event_phase'] ?? '',
+            $user['stripe']['ticket_name'] ?? '',
+            $user['stripe']['ticket_price_id'] ?? '',
         );
     }
 }
