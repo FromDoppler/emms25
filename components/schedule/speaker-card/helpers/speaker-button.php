24b4adb724c08716e45ab6dabd65733cb83ca09e
Matías Roy Rodriguez <mroy@makingsense.com>
2025-10-07 16:13:12 -0300

    fix: remove button in speaker card

--- components/schedule/speaker-card/helpers/speaker-button.php
+++ components/schedule/speaker-card/helpers/speaker-button.php
@@ -30,6 +30,12 @@ function getExposeButtonData($speaker)
   ];
 }
 
+function shouldHideButton(array $speaker, bool $isRegistered, string $currentPath, string $targetPath = '/checkout-lp-landing'): bool
+{
+    $isVipButton = $isRegistered && ($speaker['exposes'] === 'workshop');
+    return $currentPath === $targetPath && !$isVipButton;
+}
+
 
 function getExposeButtonDataRegistered($speaker, $eventFase = 'pre')
 {
