26916c3387cb95236941cea15aad24979c108e66
Matías Roy Rodriguez <mroy@makingsense.com>
2025-10-22 13:24:47 -0300

    feat: add home post layout base

--- pages/digital-trends/post/home-registrado.php
+++ pages/digital-trends/post/home-registrado.php
@@ -3,29 +3,29 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/utils/cacheSettings.php');
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
-    <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/ecommerce/post/home/seo-reg.php'); ?>
-    <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/head.php'); ?>
+  <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/digital-trends/post/home/head.php'); ?>
+  <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/head.php'); ?>
 </head>
 
 <body class="emms__home">
-    <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/gtm.php'); ?>
-    <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/hello-bar.php') ?>
-    <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/navbar-reg.php') ?>
-    <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/share.php') ?>
-    <main>
-        <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/ecommerce/post/home/hello-module.php');   ?>
-        <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/ecommerce/post/central-video.php'); ?>
-        <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/ecommerce/post/premium-content.php') ?>
-        <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/ecommerce/post/event-numbers.php') ?>
-        <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/ecommerce/post/speakers-carousel.php') ?>
-        <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/ecommerce/post/users-comments.php') ?>
-        <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/ecommerce/post/faqs.php') ?>
-        <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/ecommerce/post/companies-list.php') ?>
-    </main>
-    <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/footer.php'); ?>
+  <?php if (PRODUCTION) include $_SERVER['DOCUMENT_ROOT'] . '/components/gtm.php'; ?>
+  <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/hello-bar.php') ?>
+  <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/navbar-reg.php') ?>
+  <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/share.php') ?>
+  <main>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/digital-trends/post/home/hello-module.php') ?>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/digital-trends/post/central-video.php');    ?>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/digital-trends/post/premium-content.php') ?>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/digital-trends/post/event-numbers.php') ?>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/digital-trends/post/speakers-carousel.php') ?>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/digital-trends/post/users-comments.php') ?>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/digital-trends/post/faqs.php') ?>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/sponsorsList.php') ?>
+  </main>
+  <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/footer.php'); ?>
 </body>
 
 </html>
