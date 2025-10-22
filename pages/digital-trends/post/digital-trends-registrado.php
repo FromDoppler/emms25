6a1ec58b097def5f98315d681e2ebfaacbe00f57
Matías Roy Rodriguez <mroy@makingsense.com>
2025-10-22 17:02:56 -0300

    feat: add base layout for digital trends post

--- pages/digital-trends/post/digital-trends-registrado.php
+++ pages/digital-trends/post/digital-trends-registrado.php
@@ -1,56 +1,63 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'] . '/config.php');
 require_once($_SERVER['DOCUMENT_ROOT'] . '/utils/cacheSettings.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/components/modal/modal.php');
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
-    <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/ecommerce/post/ecommerce/seo-reg.php'); ?>
-    <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/head.php'); ?>
-    <script type="module">
-        import {
-            hiddenOrShowUserUI
-        } from '/src/<?= VERSION ?>/js/user.js';
-        import {
-            toggleVipEcommerceElements
-        } from '/src/<?= VERSION ?>/js/toggleVipElements.js';
-        import {
-            eventsType
-        } from '/src/<?= VERSION ?>/js/enums/eventsType.enum.js';
-        hiddenOrShowUserUI(eventsType.ECOMMERCE);
-        toggleVipEcommerceElements()
-    </script>
+  <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/digital-trends/post/digital-trends/head.php'); ?>
+  <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/head.php'); ?>
+  <script type="module">
+    import {
+      hiddenOrShowUserUI
+    } from '/src/<?= VERSION ?>/js/user.js';
+    hiddenOrShowUserUI(window.APP.EVENTS.CURRENT.freeId);
+  </script>
+  <script type="module">
+    import {
+      toggleVipDigitalTrendsElements
+    } from '/src/<?= VERSION ?>/js/toggleVipElements.js';
+    toggleVipDigitalTrendsElements();
+  </script>
 </head>
 
-<body class="emms__ecommerce emms__ecommerce-logueado">
-    <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/gtm.php'); ?>
+<body>
+  <div class="register-form__container hidden--vip">
     <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/hello-bar.php'); ?>
-    <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/navbar-reg.php') ?>
-    <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/share.php') ?>
-    <main>
-        <div class="hidden--vip ">
-            <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/ecommerce/post/ecommerce/hello-module.php') ?>
-        </div>
-        <div class="show--vip">
-            <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/ecommerce/post/ecommerce/hello-vip-module.php') ?>
-        </div>
-        <div class="emms__bg-dark-gradient">
-            <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/schedule/schedule.php') ?>
-        </div>
-        <div class="hidden--vip">
-            <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/ecommerce/post/ecommerce/entry-plans.php') ?>
-        </div>
+  </div>
 
-        <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/ecommerce/post/premium-content.php') ?>
-        <!-- Academy Banner === show--vip, This class is not used to prevent interfering with flickity -->
-        <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/sponsorsList.php') ?>
-        <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/ecommerce/post/doppler-academy-banner.php'); ?>
-    </main>
-    <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/footer.php'); ?>
-    <script src="src/<?= VERSION ?>/js/newDate.js" type="module"></script>
+  <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/gtm.php'); ?>
+  <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/navbar-reg.php') ?>
+  <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/share.php') ?>
+  <main>
+    <div class="show--vip">
+      <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/digital-trends/post/digital-trends/hello-vip-module.php') ?>
+    </div>
+    <div class="hidden--vip">
+      <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/digital-trends/post/digital-trends/hello-module.php') ?>
+    </div>
+    <div class="gold-schedule">
+      <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/schedule/schedule.php') ?>
+    </div>
+    <div class="hidden--vip">
+      <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/digital-trends/post/digital-trends/entry-plans.php') ?>
+    </div>
+
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/digital-trends/post/premium-content.php') ?>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/sponsorsList.php') ?>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/academyBanner.php'); ?>
+  </main>
+  <?php
+  require_once($_SERVER['DOCUMENT_ROOT'] . '/components/modal/extraDataCaptor.php');
+  render_modal('modalVip', 'vipmodal',  'vip', true);
+  ?>
+  <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/footer.php'); ?>
+  <script src="src/<?= VERSION ?>/js/newDate.js" type="module"></script>
 
 </body>
+
 
 </html>
